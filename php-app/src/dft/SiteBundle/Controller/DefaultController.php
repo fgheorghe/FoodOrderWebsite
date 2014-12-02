945ee1b7df64277ef1d8d20e93a706878d80ca88
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2014-12-02 10:54:17 +0000

    Added default route.

--- php-app/src/dft/SiteBundle/Controller/DefaultController.php
+++ php-app/src/dft/SiteBundle/Controller/DefaultController.php
@@ -6,8 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('dftSiteBundle:Default:index.html.twig', array('name' => $name));
+        // TODO: Implement.
+        return $this->render('dftSiteBundle:Default:index.html.twig');
     }
 }
