e70b5d869aec2d1c50d26d6b01f5b37c3ce3666c
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2014-12-27 23:50:53 +0000

    Partial contact form logic.

--- php-app/src/dft/SiteBundle/Controller/ContactController.php
+++ php-app/src/dft/SiteBundle/Controller/ContactController.php
@@ -6,7 +6,22 @@ class ContactController extends BaseController
 {
     public function indexAction()
     {
-        // TODO: Implement.
-        return $this->render('dftSiteBundle:Contact:contact.html.twig');
+        $errorMessage = "";
+        // If the user submitted some form data, send an email to the administrator.
+        if ($this->getRequest()->isMethod('POST')) {
+            $errorMessage = "Your message has been sent. We will contact you shortly.";
+            // _POST values.
+            $request = $this->container->get("request");
+
+            $name = $request->get('name');
+            $email = $request->get('email');
+            $phoneNumber = $request->get('phone_number');
+            // TODO: Send email.
+        }
+
+        return $this->render('dftSiteBundle:Contact:contact.html.twig', array(
+                "error_message" => $errorMessage
+            )
+        );
     }
 }
