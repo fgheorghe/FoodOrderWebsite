740ef2f5897d38a2181fa92e4aa00e01f8aa06d1
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2015-01-26 15:57:54 +0000

    Upper case post codes.

--- php-app/src/dft/SiteBundle/Services/FormValidators.php
+++ php-app/src/dft/SiteBundle/Services/FormValidators.php
@@ -34,7 +34,7 @@ class FormValidators {
      * @return bool
      */
     public function isValidUkPostCode($postCode) {
-        return preg_match('#^(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2})$#', $postCode) ? true : false;
+        return preg_match('#^(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2})$#', strtoupper($postCode)) ? true : false;
     }
 
     /**
